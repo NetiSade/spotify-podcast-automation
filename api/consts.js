4e99258d9c3d565af442490600f6c52b55bf6c6c
NetiSade <netisade@gmail.com>
2025-02-14 21:39:25 +0200

    Create consts.js

---
+++ api/consts.js
@@ -0,0 +1,26 @@
+export const MAX_EPISODES_PER_SHOW = 5;
+export const OLD_EPISODES_DAYS = 14;
+export const NEW_SHOWS_LIMIT = 1;
+
+export const MY_SHOWS = [
+  {
+    name: "How To Do Things",
+    url: "https://open.spotify.com/show/3JVB81Ce5eXH8dgVKe6A57?si=abc123",
+  },
+  {
+    name: "היידה",
+    url: "https://open.spotify.com/show/0VeR5mYtFCTfSCa7SCVH83?si=def456",
+  },
+  {
+    name: "וויקליסינק",
+    url: "https://open.spotify.com/show/674Fd3udoDREXmBq44dHWY?si=dc3279b7d877434b",
+  },
+  {
+    name: "OnePlusOne",
+    url: "https://open.spotify.com/show/1jMmrLogjWyQEYPDHf5INh?si=xyz789",
+  },
+  {
+    name: "הקרנף",
+    url: "https://open.spotify.com/show/6bcWODxao3AI48YzWpF6g5?si=1234567890",
+  },
+];
